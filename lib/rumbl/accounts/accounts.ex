05e91fb15d1ee45afb48d4fe597b903472f77c5a
Cristine Guadelupe <cristineguadelupe@me.com>
2020-06-18 00:58:16 -0300

    Finding users by params (fixing typo)

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -18,7 +18,7 @@ defmodule Rumbl.Accounts do
     end
 
     def get_user_by(params) do
-        Enum.find(list_users(), fn -> map
+        Enum.find(list_users(), fn map ->
             Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
         end)
     end
