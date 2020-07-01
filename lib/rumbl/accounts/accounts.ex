948dec7188c2774723c9ec3273e2f7972af13ab2
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-01 00:23:33 -0300

    Users schema

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -13,4 +13,12 @@ defmodule Rumbl.Accounts do
         Repo.get!(User, id)
     end
 
+    def get_user_by(params) do
+        Repo.get_by!(User, params)
+    end
+
+    def list_users do
+        Repo.all(User)
+    end
+
 end
