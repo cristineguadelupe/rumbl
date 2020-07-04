81f38a40109f250fa800c6b8e6c34961b13c0fd6
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-04 02:59:05 -0300

    Player script

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,6 +2,11 @@ let Player = {
     player: null,
 
     init(domId, playerId, onReady) {
-
-    }
+        window.onYouTubeIframeAPIReady = () => {
+            this.onIframeReady(domId, playerId, onReady)
+        }
+        let youtubeScriptTag = document.createElement("script")
+        youtubeScriptTag.src = "//www.youtube.com/iframe_api"
+        document.head.appendChild(youtubeScriptTag)
+    },
 }
