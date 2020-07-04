911f1a1a8c3b05406e272be12aa0ca88781734d5
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-04 03:27:41 -0300

    Fixing a typo on youtube url regex

--- lib/rumbl_web/views/watch_view.ex
+++ lib/rumbl_web/views/watch_view.ex
@@ -2,7 +2,7 @@ defmodule RumblWeb.WatchView do
   use RumblWeb, :view
 
   def player_id(video) do
-    ~r{ˆ.*(?:youtu\.be/|\w+/lv=)(?<id>[^#&?]*)}
+    ~r{^.*(?:youtu\.be/|\w+/|v=)(?<id>[^#&?]*)}
     |> Regex.named_captures(video.url)
     |> get_in(["id"])
   end
