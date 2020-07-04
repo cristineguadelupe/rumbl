a1732108e776e84a6671df384bf9f3ec42721cf6
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-03 21:48:18 -0300

    Fixtures for tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,25 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Rumbl.Repo, :manual)
+
+def user_fixture(attrs \\ %{}) do
+  {:ok, user} =
+    attrs
+    |> Enum.into(%{
+      name: "Some User",
+      username: "user#{System.unique_integer([:positive])}"
+      password: attrs[:password] || "supersecret"
+    })
+    |> Accounts.register_user()
+end
+
+def video_fixture(%Accounts.User{} = users, attrs \\ %{}) do
+  attrs =
+    Enum.into(attrs, %{
+      title: "A title",
+      url: "http://example.com",
+      description: "a description"
+    })
+
+    {:ok, video} = Multimedia.create_video(user, attrs)
+    video
+end
