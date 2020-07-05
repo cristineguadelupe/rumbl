ba629b6128e1a38d7a521818b80a1e13b9981958
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-05 00:36:46 -0300

    Persisting annotations

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -2,8 +2,11 @@ defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
   end
+
+
+  
 
   def handle_in("new_annotation", params, socket) do
     broadcast!(socket, "new_annotation", %{
