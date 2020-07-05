a25a107668b57820d479e627b00e33e94cc3293a
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-04 23:34:37 -0300

    Handling messages

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -2,15 +2,11 @@ defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
     {:ok, socket}
   end
 
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
-    push(socket, "ping", %{count: count})
-
-    {:noreply, assign(socket, :count, count + 1)}
+  def handle_in("new_annotation", params, socket) do
+    
   end
 
 end
