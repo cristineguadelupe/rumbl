89b1d317b9325ab909946add3264377cf51e4dcf
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-05 00:34:08 -0300

    Persisting annotations

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -139,4 +139,13 @@ defmodule Rumbl.Multimedia do
     |> Repo.insert()
   end
 
+  def list_annotations(%Video{} = video) do
+    Repo.all(
+      from a in Ecto.assoc(video, :annotations),
+        order_by: [asc: a.at, asc: a.id],
+        limit: 500,
+        preload: [:user]
+    )
+  end
+
 end
