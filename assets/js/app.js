7869a240c8d32639aa0bbb7b482e92f1889ab788
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-04 22:20:03 -0300

    Video channel

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,12 +18,3 @@ import socket from "./socket"
 import Video from "./video"
 
 Video.init(socket, document.getElementById("video"))
-
-// import Player from "./player"
-// let video = document.getElementById("video")
-
-// if(video) {
-//     Player.init(video.id, video.getAttribute("data-player-id"), () => {
-//         console.log("player ready!")
-//     })
-// }
