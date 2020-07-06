75dcf7e8f74d68f72ca11063a97823f7670b1a9e
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-06 02:17:00 -0300

    Template

--- lib/rumbl_web/endpoint.ex
+++ lib/rumbl_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule RumblWeb.Endpoint do
     at: "/",
     from: :rumbl,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images videos js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
