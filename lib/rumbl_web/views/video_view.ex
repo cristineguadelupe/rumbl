dfede2e3587941ac3cafa9d5de8e91f97a5c5fef
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-07 20:04:31 -0300

    Inflex for annotations

--- lib/rumbl_web/views/video_view.ex
+++ lib/rumbl_web/views/video_view.ex
@@ -1,7 +1,16 @@
 defmodule RumblWeb.VideoView do
   use RumblWeb, :view
 
+  import Inflex
+
   def category_select_options(categories) do
     for category <- categories, do: {category.name, category.id}
   end
+
+  def number_of_annotations(video) do
+    annotations = length(video.annotations)
+    inflect = inflect("annotations", annotations)
+    "#{annotations} #{inflect}"
+  end
+
 end
